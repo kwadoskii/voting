e4dc0d856c7781b3445db158a2d275eb61a3ca7b
= <=>
2019-06-25 12:36:19 +0100

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.master')
+
+@section('title')
+    Welcome
+@endsection
+@section('section')
+    <div class="container-fluid">
+        <div class="row no-gutters col-lg-6" id="vimg">
+
+        </div>
+        <div class="row no-gutters col-lg-6"></div>
+    </div>
+@endsection
