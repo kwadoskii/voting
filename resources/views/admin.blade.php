91f282b29df22b8b4e9b7bf0b797e1ac4e6b5c3f
= <=>
2019-07-01 13:05:07 +0100

    Added admin console 2

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -9,11 +9,12 @@
         <div class="text-center mb-4">
             <img class="mb-4" src="images/ballot.png" alt="" width="72" height="72">
             <h1 class="h3 mb-3 font-weight-normal">Admin Portal</h1>
-            <p>Build form controls with floating labels via the <code>:placeholder-shown</code> pseudo-element. <a href="https://caniuse.com/#feat=css-placeholder-shown">Works in latest Chrome, Safari, and Firefox.</a></p>
+            <p>This portal is strictly for Administrators only. <br>If you are a voter please click <a href="#">Here </a> to login to the Voting portal.</a></p>
         </div>
 
         <div class="form-label-group">
-            <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required="" autofocus="">
+            <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required=""
+                   autofocus="">
             <label for="inputEmail">Email address</label>
         </div>
 
@@ -22,11 +23,6 @@
             <label for="inputPassword">Password</label>
         </div>
 
-        <div class="checkbox mb-3">
-            <label>
-                <input type="checkbox" value="remember-me"> Remember me
-            </label>
-        </div>
-        <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+        <button class="mt-4 btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
     </form>
 @endsection
