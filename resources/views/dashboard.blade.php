ecee908e84c218262365bdbdb30ca2251025ac5d
= <=>
2019-07-30 11:57:57 +0100

    fixed bug on dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -22,22 +22,44 @@ Admin Dashboard
 
 <nav class="navbar navbar-expand-md navbar-dark bg-dark">
     <div class="container-fluid my-2">
-        <a class="navbar-brand" href="{{ route('admindashboard') }}">Admin Panel</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse">
+        <a class="navbar-brand col-md-2" href="{{ route('admindashboard') }}">Admin Panel</a>
+
+        <div class="btn btn-success ml-auto mr-2">{{ Auth::guard('admin')->user()->first_name}}</div>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapses">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="navbar-collapse collapse" id="navbarCollapse">
-            <ul class="navbar-nav px-3 ml-md-auto">
-                <li class="btn">
-                    <p class="btn btn-success">{{ Auth::guard('admin')->user()->first_name}}</p>
-                </li>
-                <li class="nav-item text-nowrap">
-                    <a class="nav-link" href="{{ route('adminlogout') }}">Sign out</a>
+        <div class="navbar-collapse collapse" id="navbarCollapses">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('adminlogout') }}">Sign Out</a>
                 </li>
             </ul>
         </div>
     </div>
 </nav>
+
+{{-- <nav class="navbar navbar-expand-sm navbar-dark bg-dark ">
+    <div class="container-fluid my-2">
+        <a class="navbar-brand " href="#">Admin Panel</a>
+
+        <div class="btn ggg bg-success ml-auto mr-2">Name</div>
+
+
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="navbar-collapse collapse" id="navbarCollapse">
+
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="nav-item nav-link" href="#">Sign out</a>
+                </li>
+            </ul>
+        </div>
+
+    </div>
+</nav> --}}
 
 <div class="container-fluid">
     <div class="row">
@@ -202,20 +224,18 @@ Admin Dashboard
     </div>
 </div>
 
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-    integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
-</script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/feather-icons/4.9.0/feather.min.js"></script>
-{{-- <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.3/Chart.min.js"></script> --}}
 <script src="../src/main.js"></script>
 
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+{{-- <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.3/Chart.min.js"></script> --}}
+{{-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script> --}}
+{{-- <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
     integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
-</script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+</script> --}}
+{{-- <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
     integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
-</script>
+</script> --}}
+
 <script>
     var token = '{{ Session::token() }}';
         var urlAddAdmin = '{{ route('addadmin') }}';
