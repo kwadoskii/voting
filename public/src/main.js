baa4c27f555c7f2c42429c868360860bf184cafa
= <=>
2019-12-11 17:05:28 +0100

    table click to sort

--- public/src/main.js
+++ public/src/main.js
@@ -613,6 +613,45 @@ $(document).on('click', deletes, function (e) {
     });
 }
 
+{
+    $(document).on('click', '#candidateSearch', function(e){
+        console.log('yes');
+    });
+
+    //Credit from http://jsfiddle.net/zscQy/
+    function sortTable(table, col, reverse) {
+        var tb = table.tBodies[0], // use `<tbody>` to ignore `<thead>` and `<tfoot>` rows
+            tr = Array.prototype.slice.call(tb.rows, 0), // put rows into array
+            i;
+        reverse = -((+reverse) || -1);
+        tr = tr.sort(function (a, b) { // sort rows
+            return reverse // `-1 *` if want opposite order
+                * (a.cells[col].textContent.trim() // using `.textContent.trim()` for test
+                    .localeCompare(b.cells[col].textContent.trim())
+                   );
+        });
+        for(i = 0; i < tr.length; ++i) tb.appendChild(tr[i]); // append each row in order
+    }
+
+    function makeSortable(table) {
+        var th = table.tHead, i;
+        th && (th = th.rows[0]) && (th = th.cells);
+        if (th) i = th.length;
+        else return; // if no `<thead>` then do nothing
+        while (--i >= 0) (function (i) {
+            var dir = 1;
+            th[i].addEventListener('click', function () {sortTable(table, i, (dir = 1 - dir))});
+        }(i));
+    }
+
+    function makeAllSortable(parent) {
+        parent = parent || document.body;
+        var t = parent.getElementsByTagName('table'), i = t.length;
+        while (--i >= 0) makeSortable(t[i]);
+    }
+
+    window.onload = function () {makeAllSortable();};
+}
 
 // candiate search control to hide the state and constituency Dropdowns
 {
@@ -637,7 +676,7 @@ $(document).on('click', deletes, function (e) {
     });
 }
 
-//for controlling the multiple voters for an particular party in a particular office
+//for controlling the multiple voters for a particular party in a particular office
 {
     $(document).on('change', '#office', function(e){
         let isconsti = $(this).find(':selected').data('isconsti');
