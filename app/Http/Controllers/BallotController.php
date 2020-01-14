156269e34fa5fc06da771e072f05dbab202aa4ea
Austin Ofor <kwadoskii@live.com>
2020-01-15 00:13:33 +0100

    Fixed bug on constitutional sidebar

--- app/Http/Controllers/BallotController.php
+++ app/Http/Controllers/BallotController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Office;
+use App\Lga;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -10,6 +11,6 @@ class BallotController extends Controller
 {
     public function getBallot()
     {
-        return view('ballot')->with(['offices' => Office::all()]);
+        return view('ballot')->with(['offices' => Office::all(), 'Lga' => Lga::all()]);
     }
 }
